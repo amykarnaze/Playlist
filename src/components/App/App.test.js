355c54a69b169ee12ab543715ac225c697f7f4e9
Amy Karnaze <akarnaze@gmail.com>
2020-09-02 12:06:57 -0600

    Begin test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,3 +5,12 @@ import App from './App';
 test('true should be true', () => {
   expect(true).toEqual(true);
 });
+
+describe('App', () => {
+  it('should add a song', () => {
+    const addSong = jest.fn();
+    render(<App addSong={addSong}/>
+      
+    )
+  })
+})
