6a1064215292efa57b9f1cf450ed5cd481d624c9
Amy Karnaze <akarnaze@gmail.com>
2020-09-02 10:24:12 -0600

    Create basic form compnent and render structure

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -11,8 +11,29 @@ class Form extends Component {
 
   render() { 
     return (
-      
+      <section>
+        <input className="song-Name"
+          type="text"
+          id="songName"
+          name="songName"
+        /> 
+        <input className="artist-name"
+          type="text"
+          id="artistName"
+          name="artistName"
+          placeholder="name"
+        />
+        <input className="link"
+          type="text"
+          id="link"
+          name="link"
+          placeholder="link"
+        />
+        <button type="submit">Add Song</button>
+      </section>
     )
   
   }
 }
+
+export default Form;
