2773d8ebe6035254e608921105df6d066d9a783f
Amy Karnaze <akarnaze@gmail.com>
2020-09-02 10:12:18 -0600

    Create song component that maps over songs and passes info to card

---
+++ src/components/Songs/Songs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const Songs = ( {songQueue} ) => {
+  // console.log({SQ:songQueue})
+  return (
+    <section>
+      <h2>Your Songs</h2>
+        {songQueue.map(song => {
+          
+           return <Card
+            songName={song.songName}
+            artistName={song.artistName}
+            link={song.link}
+            id={song.id}
+          />
+        })
+      }
+    </section>
+  )
+}
+
+export default Songs
