bd458ab28fdfd6fcabd71d1e2b153628a97ae13b
Amy Karnaze <akarnaze@gmail.com>
2020-09-02 10:12:27 -0600

    Create Card component that displays song info

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 const Card = ( {songName, artistName, link, id}) => {
-  
+  return (<section>
+    <h2>Song: {songName}</h2>
+    <h3>Artist: {artistName}</h3>
+    <h4>Music Video: {link}</h4>
+  </section>
+  )
 }
+
+export default Card;
