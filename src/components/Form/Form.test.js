b0b6568ef176d84091c7bb180767b0c87ab16c2c
Amy Karnaze <akarnaze@gmail.com>
2020-09-02 11:49:41 -0600

    Add test to check what inputs hold

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -18,4 +18,25 @@ describe('Form Component', () => {
     expect(submit).toBeInTheDocument();
   })
 
+    it('should check the inputs have text', async () => {
+      render(<Form />)
+
+      const songInput = screen.getByPlaceholderText("song name")
+      const artistInput = screen.getByPlaceholderText("artist name")
+      const link = screen.getByPlaceholderText("link")
+      const submit = screen.getByRole('button', {name: "Add Song"})
+
+      expect(songInput).toBeInTheDocument();
+      expect(artistInput).toBeInTheDocument();
+      expect(link).toBeInTheDocument();
+      expect(submit).toBeInTheDocument();
+
+      fireEvent.change(songInput, {target: {name: "songName", value: 'hello'}})
+      expect(songInput.value).toEqual('hello');
+      fireEvent.change(artistInput, {target: {name: "artistName", value: 'hi'}})
+      expect(artistInput.value).toEqual('hi');
+      fireEvent.change(link, {target: {name: "link", value: 'here'}})
+      expect(link.value).toEqual('here');
+    })
+
 })
